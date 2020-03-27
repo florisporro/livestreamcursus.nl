47655768f395a83eb4d70a8876dc2025a8223cae
florisporro <floris@florisporro.com>
2020-03-27 18:23:25 +0100

    Update gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -32,26 +32,6 @@ module.exports = {
 			}
 		}
 	],
-	templates: {
-		Blog: [
-			{
-				path: "/blog/:title",
-				component: "./src/templates/BlogEntry.vue"
-			}
-		],
-		Category: [
-			{
-				path: "/category/:title",
-				component: "./src/templates/Category.vue"
-			}
-		],
-		Tag: [
-			{
-				path: "/tag/:title",
-				component: "./src/templates/Tag.vue"
-			}
-		]
-	},
 	chainWebpack: config => {
 		config.resolve.alias.set("@modules", "@/resources/js/modules");
 		config.resolve.alias.set("@utilities", "@/resources/js/utilities");
